c9c6c986d8ee3ba30520e3f660e6063c20d8608e
vegaliu <vegaliu@foxmail.com>
2022-10-19 14:19:07 +0000

    显示头像

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -8,10 +8,8 @@
 
     <div class="col-lg-3 col-md-3 hidden-sm hidden-xs user-info">
       <div class="card ">
-        <img class="card-img-top"
-          src="https://img2.woyaogexing.com/2022/10/19/23279d969135feca!400x400.png"
-          alt="{{ $user->name }}">
-          <div class="card-body">
+        <img class="card-img-top" src="{{ $user->avatar }}" alt="{{ $user->name }}">
+        <div class="card-body">
             <h5><strong>个人简介</strong></h5>
             <p>{{ $user->introduction }}</p>
             <hr>

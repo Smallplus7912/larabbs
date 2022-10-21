f1d580f956d9c1337ab7e72f74e4d86a543ec340
vegaliu <vegaliu@foxmail.com>
2022-10-21 13:10:10 +0000

    假数据填充

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    public function run()
+    {
+        // 生成数据集合
+        User::factory()->count(10)->create();
+
+        // 单独处理第一个用户的数据
+        $user = User::find(1);
+        $user->name = 'Summer';
+        $user->email = 'summer@example.com';
+        $user->avatar = 'https://cdn.learnku.com/uploads/avatars/104403_1666334985.jpeg!/both/400x400';
+        $user->save();
+    }
+}

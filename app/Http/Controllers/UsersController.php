686d915c4e65e9fc491749493701e972704bfac2
vegaliu <vegaliu@foxmail.com>
2022-10-21 06:25:24 +0000

    授权访问

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -15,11 +15,13 @@ class UsersController extends Controller
     }
 
     public function edit(User $user){
+        $this->authorize('update', $user);
         return view('users.edit',compact('user'));
     }
 
     public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
+        $this->authorize('update', $user);
         $data = $request->all();
         if ($request->avatar) {
             $result = $uploader->save($request->avatar, 'avatars', $user->id);
@@ -30,5 +32,9 @@ class UsersController extends Controller
 
         $user->update($data);
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
+    }
+
+    public function __construct(){
+        $this->middleware('auth',['except'=>['show']]);
     }
 }

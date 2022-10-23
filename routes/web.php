3cf73bc1054be515e037fda42b50a6b67dd256fd
vegaliu <vegaliu@foxmail.com>
2022-10-23 02:27:07 +0000

    上传图片

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,6 @@ Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'crea
 
 //话题分类列表
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+
+//上传图片路由
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');

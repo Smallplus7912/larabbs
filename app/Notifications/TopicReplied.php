5393d0a80257ca9419b5814e83f43ee709416274
vegaliu <vegaliu@foxmail.com>
2022-11-03 03:33:14 +0000

    邮件通知，队列

--- app/Notifications/TopicReplied.php
+++ app/Notifications/TopicReplied.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use App\Models\Reply;
 
-class TopicReplied extends Notification
+class TopicReplied extends Notification implements ShouldQueue
 {
     use Queueable;
 
@@ -22,8 +22,18 @@ class TopicReplied extends Notification
 
     public function via($notifiable)
     {
-        // 开启通知的频道
-        return ['database'];
+        // 开启通知的频道，mail为邮件通知
+        return ['database', 'mail'];
+    }
+
+    //因为开启了mail频道，需要新增toMali方法
+    public function toMail($notifiable)
+    {
+        $url = $this->reply->topic->link(['#reply' . $this->reply->id]);
+
+        return (new MailMessage)
+                    ->line('你的话题有新回复！')
+                    ->action('查看回复', $url);
     }
 
     public function toDatabase($notifiable)
